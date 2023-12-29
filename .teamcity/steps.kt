04d8386ba2cf2d4bcfa31fcff1b265568937e1c8
James Springall <james_springall@hotmail.com>
2023-12-30 09:21:17 +1100

    no message

---
+++ .teamcity/steps.kt
@@ -0,0 +1,28 @@
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.ExecBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+object CommonSteps {
+
+    fun BuildType.printPullRequestNumber(
+    ) {
+        steps {
+            script {
+                name = "Settings 2 Print Pull Request Number"
+                scriptContent = """
+                #!/bin/bash
+                id=%teamcity.pullRequest.number%
+                echo "Id is: ${'$'}id"
+                branch="pull/${'$'}id"
+                echo "Branch is: ${'$'}branch"
+            """.trimIndent()
+            }
+        }
+    }
+
+
+}
